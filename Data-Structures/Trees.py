92a13af952ae68e4bebf5c78aab7bc4c530ce128
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2024-08-23 22:52:50 -0400

    Create Trees.py

---
+++ Data-Structures/Trees.py
@@ -0,0 +1,26 @@
+"""
+A tree is:
+- Empty, or
+- A node with:
+--- a key, amd
+--- a list of child trees
+
+Root: top node in the tree.
+Leaf: Node with no children
+Level: 1 + number of edges between
+a root and the node
+Height: length of the longest path 
+from the root to a leaf.
+Forest: Collection of trees
+
+Binary Tree:(each node has at most two children)
+-key
+-left
+-right
+-(optional) parent
+
+"""
+
+
+
+
