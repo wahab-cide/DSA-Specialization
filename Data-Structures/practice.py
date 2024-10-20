5731f885e4250058168a4e8cc29f2d92a0ca3c5a
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2024-10-20 01:53:18 -0400

    add practice problems

--- Data-Structures/practice.py
+++ Data-Structures/practice.py
@@ -5,100 +5,38 @@ class minHeap:
 
     def insert(self, element):
         self.heap.append(element)
-        self.heap._heapify_up(len(self.heap) - 1)
+        self._heapify_up(element)
 
-    def extractMax(self):
+    def extractMin(self):
         if not self.heap:
             raise IndexError('Heap is empty')
         if len(self.heap) == 1:
             return self.heap.pop()
-        
         root = self.heap[0]
-
         self.heap[0] = self.heap.pop()
-        self._heapify_up(0)
+        self._heapify_down(0)
         return root
     
-
-
     def _heapify_up(self, index):
-        parent_index = (index - 1) // 2
+        parent_index =  (index - 1) // 2
+
 
         if index > 0 and self.heap[index] < self.heap[parent_index]:
             self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
-            self._heapify_up[index]
+            self._heapify_up[parent_index]
 
     def _heapify_down(self, index):
         smallest = index
-        left_child_index = 2 + index + 1
-        right_child_index = 2 * index + 2
+        left_index = 2 * index + 1
+        right_index = 2 * index + 2
 
 
-        if left_child_index < len(self.heap) and self.heap[left_child_index] < self.heap[index]:
-            smallest = left_child_index
+        if left_index < len(self.heap) and self.heap[left_index] < self.heap[smallest]:
+            smallest = left_index
 
-        if right_child_index < len(self.heap) and self.heap[right_child_index] < self.heap[index]:
-            smalest = right_child_index
+        if right_index < len(self.heap) and self.heap[right_index] < self.heap[smallest]:
+            smallest = right_index
 
         if smallest != index:
             self.heap[smallest], self.heap[index] = self.heap[index], self.heap[smallest]
-            self._heapify_down(smallest)
-
-
-
-    
-
-    def heapify(self, arr, n, i):
-        largest = i
-        left_index = 2 * 1 + 1
-        right_index = 2 * 1 + 2
-
-
-
-        if left_index < n and arr[left_index] > arr[largest]:
-            largest = left_index
-
-        if right_index < n and arr[right_index] > arr[largest]:
-            largest = right_index
-
-        if largest != i:
-            arr[largest], arr[i] = arr[i], arr[largest]
-            self.heapify(arr, n, largest)
-
-
-    def heapsort(self, arr):
-        n = len(arr)
-        for i in range(n // 2 - 1, -1, -1):
-            self.heapify(arr, n, i)
-
-        for i in range(n - 1, 0, -1):
-            arr[i], arr[0] = arr[0], arr[i]
-            self.heapify(arr, i, 0)
-
-
-# find Kth largest element
-
-import heapq
-
-def find_kth_largest(nums, k):
-    min_heap = nums[:k]
-
-    heapq.heapify(min_heap)
-
-
-    for num in nums[k:]:
-        if num > min_heap[0]:
-            heapq.heappop(min_heap)
-            heapq.heappush(min_heap, num)
-
-    return min_heap[0]
-
-
-
-def merge_k_sorted_lists(lists):
-    min_heap = []
-
-    for i, l in enumerate(lists):
-        if l:
-            heapq.heappush(min_heap, (l[0], i, 0))
-        
+            self._heapify_down[smallest]
---
+++ leetcode150.py
@@ -0,0 +1,58 @@
+#remove duplicates  I 26
+def removeDuplicates(nums):
+    # Edge case: If the array is empty, return 0
+    if not nums:
+        return 0
+
+    # Initialize the pointer for the unique elements
+    k = 1
+
+    # Iterate through the array starting from the second element
+    for i in range(1, len(nums)):
+        # If the current element is different from the previous element
+        if nums[i] != nums[i - 1]:
+            # Place it at the next unique position
+            nums[k] = nums[i]
+            k += 1
+    
+    # Return the number of unique elements
+    return k
+
+
+#remove duplicates II 80
+def removeDuplicates(nums):
+    # Edge case: if the array is empty or has only one or two elements, return its length
+    if len(nums) <= 2:
+        return len(nums)
+    
+    # Initialize the pointer for the next position
+    k = 2  # We can always keep the first two elements since we want each element to appear at most twice
+    
+    # Iterate through the array starting from the third element
+    for i in range(2, len(nums)):
+        # Compare the current element with the element two places before it
+        if nums[i] != nums[k - 2]:
+            # If it's not equal, it means it's allowed to be placed
+            nums[k] = nums[i]
+            k += 1
+    
+    # Return the number of elements in the modified array
+    return k
+
+
+#majority element
+def majorityElement(nums):
+    # Phase 1: Boyer-Moore Voting Algorithm to find the candidate
+    count = 0
+    candidate = None
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+    
+    # Return the candidate since it's guaranteed to be the majority element
+    return candidate
