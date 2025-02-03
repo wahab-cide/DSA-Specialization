a3f3211b7f574850c678edcd610c33c6a60c3876
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2025-02-03 14:53:38 -0500

    add union find problems

--- NeetCode/graphs.py
+++ NeetCode/graphs.py
@@ -326,13 +326,71 @@ class Solution:
 #11 num connected components Union Find
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        pass
+        par = [i for i in range(n)]
+        rank =[1] * n
+
+        def find(n):
+            res = n
+
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return 0
+            
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return 1
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+
+        return res
+
 
 
 #12 redundant connection
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        pass
+        par = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(n):
+            res = n
+
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            
+            if rank[p1] < rank[p2]:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+
+            else:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            return True
+        
+        for n1, n2 in edges:
+            if union(n1, n2) == False:
+                return [n1, n2]
 
 #13 Word ladder
 class Solution:
