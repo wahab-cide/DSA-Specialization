30fad12844d26d99e6c38d5632b5ab64ad94a930
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2025-02-12 22:01:12 -0500

    add LC daily

--- leetCodeDaily.py
+++ leetCodeDaily.py
@@ -196,3 +196,36 @@ def maximumSum(nums):
             maxSum = max(maxSum, currSum)
 
     return maxSum
+
+
+
+"""
+You are given a 0-indexed integer array nums, and an integer k.
+
+In one operation, you will:
+
+Take the two smallest integers x and y in nums.
+Remove x and y from nums.
+Add min(x, y) * 2 + max(x, y) anywhere in the array.
+Note that you can only apply the described operation 
+if nums contains at least two elements.
+
+Return the minimum number of operations needed 
+so that all elements of the array are greater than or equal to k.
+
+"""
+from collections import heapq
+from typing import List
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        operations = 0
+        heapq.heapify(nums)
+
+        while nums[0] < k:
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            heapq.heappush(nums, (min(x, y) * 2 + max(x, y)))
+            operations += 1
+        return operations
+
+        
