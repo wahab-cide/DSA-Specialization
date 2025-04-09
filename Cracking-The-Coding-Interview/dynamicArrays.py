fe576fb11301447f1fb5cf5cd236ca4b5eb8c1dd
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2025-04-09 13:05:02 -0400

    Add Dynamic Array Problems

---
+++ Cracking-The-Coding-Interview/dynamicArrays.py
@@ -0,0 +1,43 @@
+#Data Structure Design(Dynamic Arrays)
+# Design a dynamic array class that supports the following operations:
+# 1. get(i): returns the element at index i
+# 2. set(i, x): sets the element at index i to x
+# 3. append(x): adds an element x to the end of the array
+# 4. pop_back(): removes the last element from the array
+# 5. resize(new_capacity): changes the capacity of the array to new_capacity
+class DynamicArray:
+    def __init__(self):
+        self.size = 0
+        self.capacity = 10
+        self.array = [None] * self.capacity
+
+    def get(self, i):
+        if i < 0 or i >= self.size:
+            raise IndexError("Index out of bounds")
+        return self.array[i]
+    
+    def set(self, i, x):
+        if i < 0 or i >= self.size:
+            raise IndexError("Index out of bounds")
+        self.array[i] = x
+
+    
+    def append(self, x):
+        if self.size == self.capacity:
+            self.resize(self.capacity * 2)
+        self.array[self.size] = x
+        self.size += 1
+
+    def resize(self,new_capacity):
+        new_array = [None] * new_capacity
+        for i in range(self.size):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def pop_back(self):
+        if self.size == 0:
+            raise IndexError("Pop from empty array")
+        self.size -= 1
+        if self.size /self.capacity < 0.25 and self.capacity > 10:
+            self.resize(self.capacity // 2)
