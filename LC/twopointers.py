00703a470265b0280b23b79bb1bedfa8ed188013
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2025-06-15 18:09:19 -0400

    add CWMW

--- LC/twopointers.py
+++ LC/twopointers.py
@@ -30,3 +30,35 @@ class Solution:
             left -= 1
             right += 1
         return right - left - 1
+
+
+'''
+CONTAINER WITH MOST WATER
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+'''
+
+from typing import List 
+class Solution:
+    def containerWithMostWater(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+
+        maxWater = 0
+
+        while left < right:
+            currentWater = min(height[left], height[right]) * (right - left)
+            maxWater = max(maxWater, currentWater)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return maxWater
