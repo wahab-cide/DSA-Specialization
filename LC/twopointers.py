08630443435aa25ddab548dad194699d644709bb
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2025-06-15 18:25:58 -0400

    add threeSum

--- LC/twopointers.py
+++ LC/twopointers.py
@@ -62,3 +62,42 @@ class Solution:
                 right -= 1
 
         return maxWater
+    
+
+
+'''
+3SUM
+Given an integer array nums, return all the triplets 
+[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+'''
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
+        n = len(nums)
+        res = []
+        
+
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while right > left and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return res
