8b5484830d00d9a1004d6306cb9188086e3afd36
wahab-cide <124523882+wahab-cide@users.noreply.github.com>
2025-06-15 18:48:18 -0400

    add fourSum

--- LC/twopointers.py
+++ LC/twopointers.py
@@ -74,7 +74,7 @@ Notice that the solution set must not contain duplicate triplets.
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+
         nums.sort()
         n = len(nums)
         res = []
@@ -101,3 +101,50 @@ class Solution:
                     left += 1
                     right -= 1
         return res
+
+
+'''
+fourSum
+Given an array nums of n integers, return an array of all the unique quadruplets 
+[nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+'''
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total < target:
+                        left += 1
+                    elif total > target:
+                        right -= 1
+                    else:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while right > left and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+        return res
+
+
+                
