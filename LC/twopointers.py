62df70d5732f5d4bef35d4e314882d397d194d40
wahab-cide <abdulwahabcideali@gmail.com>
2025-06-20 11:53:02 -0400

    add rotateList, sortColors

--- LC/twopointers.py
+++ LC/twopointers.py
@@ -254,3 +254,130 @@ class Solution:
                 i += 1
 
         return i
+    
+
+'''
+Next Permutation
+
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+
+'''
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        pivot = len(nums) - 2
+
+        while pivot >= 0 and nums[pivot] >= nums[pivot + 1]:
+            pivot -= 1
+
+        if pivot == -1:
+            nums.reverse()
+            return
+        
+        swap = len(nums) - 1
+        while swap > pivot and nums[swap] <= nums[pivot]:
+            swap -= 1
+
+        nums[pivot], nums[swap] = nums[swap], nums[pivot]
+
+        left, right = pivot + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -=1 
+
+    """
+    Trapping Water
+
+    Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+
+    """
+    class Solution:
+        def trap(self, height: List[int]) -> int:
+            if not height:
+                return 0
+            left, right = 0, len(height) - 1
+            left_max, right_max = height[left], height[right]
+            trapped_water = 0
+
+            while left < right:
+                if left_max < right_max:
+                    left += 1
+                    left_max = max(left_max, height[left])
+                    trapped_water += left_max - height[left]
+
+                else:
+                    right -= 1
+                    right_max = max(right_max, height[right])
+                    trapped_water += right_max - height[right]
+
+            return trapped_water
+
+"""
+Rotate List
+Given the head of a linked list, rotate the list to the right by k places.
+
+"""
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k = k % length
+        if k == 0:
+            return head 
+        
+        new_tail_pos = length - k - 1
+        new_tail = head
+
+        for _ in range(new_tail_pos):
+            new_tail = new_tail.next
+        new_head = new_tail.next
+
+        new_tail.next = None
+        tail.next = head
+
+        return new_head
+            
+
+"""
+Sort Colors
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+"""
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        left, current, right = 0, 0, len(nums) - 1
+
+        while current <= right:
+            if nums[current] == 0:
+                nums[current], nums[left] = nums[left], nums[current]
+                left += 1
+                current += 1
+            elif nums[current] == 1:
+                current += 1
+            else:
+                nums[current], nums[right] = nums[right], nums[current]
+                right -= 1
