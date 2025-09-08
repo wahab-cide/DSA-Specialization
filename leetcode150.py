2c93906cbaa2f689a5aa6d9e063a741cea0db267
Abdul Wahab Cide Ali <ac44@williams.edu>
2025-09-08 11:36:32 -0400

    add new problems

--- leetcode150.py
+++ leetcode150.py
@@ -2,6 +2,9 @@
 
 
 #remove duplicates  I 26
+from typing import List
+
+
 def removeDuplicates(nums):
     # Edge case: If the array is empty, return 0
     if not nums:
@@ -59,3 +62,195 @@ def majorityElement(nums):
     
     # Return the candidate since it's guaranteed to be the majority element
     return candidate
+
+#jump game
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+        n = len(nums)
+
+        for i in range(n):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= n - 1:
+                return True
+        return False
+
+# Jump Game II
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        
+        jumps = 0
+        curr_end = 0
+        farthest = 0
+
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == curr_end:
+                jumps += 1
+                curr_end = farthest
+                if curr_end >= n - 1:
+                    break
+
+        return jumps
+    
+    #gas station
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        total_gas = 0
+        total_cost = 0
+        current_tank = 0
+        start_idx = 0
+
+        for i in range(len(gas)):
+            total_gas += gas[i]
+            total_cost += cost[i]
+            current_tank += gas[i] - cost[i]
+            if current_tank < 0:
+                start_idx = i + 1
+                current_tank = 0
+
+        return start_idx if total_gas >= total_cost else -1
+        
+
+#Candy
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candies = [1] * n
+
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)
+    
+#Trapping Rain Water
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        left , right = 0, len(height)
+        left_max, right_max = height[left], height[right]
+        trapped = 0
+
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                trapped += left_max - height[left]
+
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                trapped += right_max - height[right]
+
+        return trapped
+    
+#Roman to integer
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanMap = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        
+        total = 0
+
+        n = len(s)
+
+        for i in range(n):
+            if i < n - 1 and romanMap[s[i]] < romanMap[s[i + 1]]:
+                total -= romanMap[s[i]]
+            else:
+                total += romanMap[s[i]]
+
+        return total
+    
+    #Value to roman
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        val_to_roman = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+        ]
+
+        ROMAN = []
+        for value, symbol in val_to_roman:
+            while value <= num:
+                ROMAN.append(symbol)
+                num -= value
+
+        return ''.join(ROMAN)
+    
+
+#Length of last word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        length = 0
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+
+        return length
+    
+
+#zigzag conversion
+'''
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+'''
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or len(s) <= numRows:
+            return s
+        
+        rows = [""] * numRows
+        current = 0
+        going_down = False
+
+        for char in s:
+            rows[current] += char
+            if current == 0 or current == numRows - 1:
+                going_down = not going_down
+            current += 1 if going_down else -1
+        return "".join(rows)
